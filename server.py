4a9a0f8cfb16013b365ea6b8ec2dda97f4f94227
aligholami30 <aligholaminezhad2679@gmail.com>
2025-04-25 15:03:27 +0330

    initial commit

---
+++ server.py
@@ -0,0 +1,21 @@
+# server.py
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+latest_command = None
+
+@app.route('/send', methods=['POST'])
+def send():
+    global latest_command
+    latest_command = request.json.get('cmd')
+    return jsonify({"status": "received", "command": latest_command})
+
+@app.route('/get', methods=['GET'])
+def get():
+    global latest_command
+    cmd = latest_command
+    latest_command = None  # بعد از دریافت، دستور پاک میشه
+    return jsonify({"command": cmd})
+
+if __name__ == '__main__':
+    app.run()
